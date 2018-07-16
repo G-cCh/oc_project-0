d3b907090fa08b6e239336f519df964bfc24b915
GC <gwenn.de-chaurand@enedis.fr>
2018-07-16 17:42:57 +0100

    Creation of 2 new products

--- src/main/java/com/lambazon/repository/ProductRepository.java
+++ src/main/java/com/lambazon/repository/ProductRepository.java
@@ -22,6 +22,8 @@ public class ProductRepository {
 		products.put(++id, new Product(id, 30, 69.99, "JVC HAFX8R Headphone", "Riptidz, In-Ear"));
 		products.put(++id, new Product(id, 40, 32.50, "VTech CS6114 DECT 6.0", "Cordless Phone"));
 		products.put(++id, new Product(id, 50, 895.00, "NOKIA OEM BL-5J", "Cell Phone "));
+		products.put(++id, new Product(id, 30, 204.99, "Samsung A3", "Smartphone"));
+		products.put(++id, new Product(id, 40, 859.00, "iphone 7", "Smartphone"));
 	}
 	
 	public List<Product> products() {
